a877f9df7c1c8a4fc644102c4a773fa811800166
Bryan Piamba <piambabryan20@gmail.com>
2023-01-14 13:36:37 -0500

    actualizacion de una variable de entorno

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv/config';
 console.log(process.env.DB_HOST);
 //127.0.0.1
 const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
-    host: '127.0.0.1',
+    host: process.env.DB_HOST,
     port: '3306',
     dialect: 'mysql',
     define:{
